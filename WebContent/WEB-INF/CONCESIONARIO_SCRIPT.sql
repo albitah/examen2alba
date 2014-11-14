ad200ce86dd939eeb77b2184c38ca33600f7ede9
1 <1@06-PC>
2014-11-14 12:17:14 +0100

    1

---
+++ WebContent/WEB-INF/CONCESIONARIO_SCRIPT.sql
@@ -0,0 +1,39 @@
+-- --------------------------------------------------------
+-- Host:                         127.0.0.1
+-- Versión del servidor:         5.0.45-community-nt - MySQL Community Edition (GPL)
+-- SO del servidor:              Win32
+-- HeidiSQL Versión:             8.0.0.4396
+-- --------------------------------------------------------
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+
+-- Volcando estructura de base de datos para concesionario
+DROP DATABASE IF EXISTS `concesionario`;
+CREATE DATABASE IF NOT EXISTS `concesionario` /*!40100 DEFAULT CHARACTER SET utf8 */;
+USE `concesionario`;
+
+
+-- Volcando estructura para tabla concesionario.vehiculo
+DROP TABLE IF EXISTS `vehiculo`;
+CREATE TABLE IF NOT EXISTS `vehiculo` (
+  `id` int(4) NOT NULL auto_increment,
+  `matricula` varchar(15) NOT NULL,
+  `marca` varchar(30) NOT NULL,
+  `modelo` varchar(4) NOT NULL,
+  `color` varchar(20) NOT NULL,
+  `numeroCaballos` int(3) NOT NULL,
+  `marchas` tinyint(5) NOT NULL,
+  PRIMARY KEY  (`id`),
+  UNIQUE KEY `matricula` (`matricula`)
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8;
+
+-- Volcando datos para la tabla concesionario.vehiculo: ~4 rows (aproximadamente)
+/*!40000 ALTER TABLE `vehiculo` DISABLE KEYS */;
+
+/*!40000 ALTER TABLE `vehiculo` ENABLE KEYS */;
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
